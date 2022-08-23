a40474aed78f84a1e08e7429973d3583b3880f38
rinehart22 <96689631+rinehart22@users.noreply.github.com>
2022-08-23 21:46:09 +0530

    Update views.py

--- app/views.py
+++ app/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 def home(request):
     return render(request, 'h.html')
+
+
+def recipes_view(request):
+    recipes = Recipe.objects.all()
+    return render(request, 'app/recipes.html', {
+        'recipes': recipes
+    })
